be2aafdc68718e20f08bb8ef0b21054b65a81ad4
Pablo <pablo.gomez@yourdigitalresource.com>
2024-12-10 17:21:22 -0500

    Fixing typos on Navbar component call

--- app/why-list-with-listy/page.js
+++ app/why-list-with-listy/page.js
@@ -1,4 +1,4 @@
-import Navbar from '@/components/navbar'
+import Navbar from '@/components/Navbar'
 import {Divider} from '@nextui-org/react'
 import Faqs from '@/components/Faqs'
 import Footer from '@/components/Footer'
